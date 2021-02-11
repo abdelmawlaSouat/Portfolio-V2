241de7da930edb6420b7d492720f25bf730e6217
Abdelmawla Souat <abdelmawla.souat@gmail.com>
2021-02-11 09:48:18 +0100

    Add Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,27 +4,26 @@
  * Email: abdelmawla.souat@gmail.com
  * -----
  * Created at: 2021-02-10 7:42:49 pm
- * Last Modified: 2021-02-10 9:31:39 pm
+ * Last Modified: 2021-02-11 9:41:38 am
  * -----
  * Copyright (c) 2021 Yuei
  */
 
-import Image from 'next/image';
+import styles from './Navbar.module.css';
+import Link from 'next/link';
 
-export default function Navbar() {
+export default function Navbar({ links }) {
   return (
     <nav>
-      <ul>
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+      <ul className={styles.linksList}>
+        {links.map((link) => (
+          <li>
+            <Link href={link.route}>
+              <a>{link.name}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
-      <Image
-        src="/images/contrast.svg"
-        alt="Switch Theme"
-        width="30"
-        height="30"
-      />
     </nav>
   );
 }
