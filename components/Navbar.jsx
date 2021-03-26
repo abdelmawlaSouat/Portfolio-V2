3a66888c38532de9a45c15b81cee12a81552457f
Abdel <abdelmawla.souat@gmail.Com>
2021-03-26 10:58:07 +0100

    Add animation to mobile navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@
  * Email: abdelmawla.souat@gmail.com
  * -----
  * Created at: 2021-02-10 7:42:49 pm
- * Last Modified: 2021-03-22 8:11:05 pm
+ * Last Modified: 2021-03-26 10:55:03 am
  * -----
  * Copyright (c) 2021 Yuei
  */
@@ -13,6 +13,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Proptypes from 'prop-types';
 import clsx from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
 import { AiOutlineClose } from 'react-icons/ai';
 import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 
@@ -27,24 +28,33 @@ export default function Navbar({ links }) {
         size="30"
         onClick={() => setVisibleNav(!visibleNav)}
       />
-      <div
-        className={clsx(styles.mobileMenu, visibleNav ? styles.visibleNav : '')}
-      >
-        <AiOutlineClose
-          size="50"
-          className={clsx(styles.svg)}
-          onClick={() => setVisibleNav(!visibleNav)}
-        />
-        <ul className={styles.linksList}>
-          {links.map((link) => (
-            <li key={link.idx}>
-              <Link href={link.route}>
-                <a>{link.name}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+
+      <AnimatePresence>
+        {visibleNav && (
+          <motion.div
+            className={clsx(styles.mobileMenu)}
+            initial={{ opacity: 0, x: -300 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -300 }}
+            transition={{ ease: 'linear', duration: 0.3 }}
+          >
+            <AiOutlineClose
+              size="50"
+              className={clsx(styles.svg)}
+              onClick={() => setVisibleNav(!visibleNav)}
+            />
+            <ul className={styles.linksList}>
+              {links.map((link) => (
+                <li key={link.idx}>
+                  <Link href={link.route}>
+                    <a>{link.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
